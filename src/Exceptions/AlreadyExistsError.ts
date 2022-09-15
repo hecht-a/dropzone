96920c6b7b6a7bde9d3860224242f312d3b44f27
hecht-a <axel.hct@gmail.com>
2022-09-15 09:16:09 +0200

    fix: typo

--- src/Exceptions/AlreadyExistsError.ts
+++ src/Exceptions/AlreadyExistsError.ts
@@ -2,6 +2,6 @@ export class AlreadyExistsError extends Error {
 	constructor(target: string) {
 		super();
 		this.name = "AlreadyExistsError";
-		this.message = `Dropzone element already exists for the element with id "${target}"`;
+		this.message = `Dropzone element already exists for the element with id "${target}."`;
 	}
 }
--- src/Exceptions/InvalidInputTypeError.ts
+++ src/Exceptions/InvalidInputTypeError.ts
@@ -2,6 +2,6 @@ export class InvalidInputTypeError extends Error {
 	constructor(inputType: string) {
 		super();
 		this.name = "InvalidInputTypeError";
-		this.message = `${inputType} type does not correspond to file type`;
+		this.message = `${inputType} type does not correspond to file type.`;
 	}
 }
